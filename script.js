50f51c038efdb9c621612795ab7085514071d095
Renata Karonlina <renatakarolinarko@gmail.com>
2022-10-09 20:10:53 -0400

    refatorandov site

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
-const btnMobile = document.querySelector('.btn-mobile')
+const btnMobile = document.querySelector(".btn-mobile");
 
 function showMenu() {
-    const nav = document.querySelector('.nav')
-    nav.classList.toggle('show')
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("show");
 
-    const links = document.querySelectorAll('nav ul li a')
+  const links = document.querySelectorAll("nav ul li a");
 
-    for (let link of links) {
-        link.addEventListener("click", function () {
-            nav.classList.remove("show")
-        })
-    }
+  for (let link of links) {
+    link.addEventListener("click", function () {
+      nav.classList.remove("show");
+    });
+  }
 }
 
-btnMobile.addEventListener('click', showMenu)
+btnMobile.addEventListener("click", showMenu);
 
 // Carrousel seção produtos
 
-const btnNext = document.querySelector('.btn-next')
+const btnNext = document.querySelector(".btn-next-rigth");
 
-let current = 0
+let current = 0;
 
-const images = document.querySelectorAll('.img-carrousel')
+const images = document.querySelectorAll(".img-carrousel");
 
 let maxCurrent = images.length - 1;
-console.log(maxCurrent) 
-
+console.log(maxCurrent);
 
 function handleClickNext() {
-    if (current < maxCurrent) {
-        current += 1
-    } else {
-        current = 0
-    }
-    console.log(current)
-    
-    images.forEach((image) => image.classList.remove('current'))
+  if (current < maxCurrent) {
+    current += 1;
+  } else {
+    current = 0;
+  }
+  console.log(current);
 
-    images[current].scrollIntoView({
-        inline:'center',
-        behavior:'smooth'
-    })
+  images.forEach((image) => image.classList.remove("current"));
 
-    images[current].classList.add('current')   
+  images[current].scrollIntoView({
+    inline: "center",
+    behavior: "smooth",
+  });
+
+  images[current].classList.add("current");
 }
 
-btnNext.addEventListener('click', handleClickNext)
+// btnNext.addEventListener("click", handleClickNext);
+
+// Theme Ligth
+
+const input = document.querySelector(".btn-theme");
+const body = document.querySelector("body");
+
+console.log(input);
+console.log(body);
+
+const toggleThemeMode = () => {
+  body.classList.toggle("light-theme");
+};
+
+input.onchange = toggleThemeMode;
